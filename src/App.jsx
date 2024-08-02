29da5f6c31ba8ee61415fb486a2025e9beef32de
Shahwaiz Islam <s1404islam@gmail.com>
2024-08-02 16:51:23 +0530

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-
 import "./App.css";
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Redux Toolkit</h1>
